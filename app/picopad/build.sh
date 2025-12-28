e55d708f99ca5bde40ed448cc0f65fac486b2d1b
shapoco <40152459+shapoco@users.noreply.github.com>
2025-12-28 10:39:54 +0900

    fix build script for PicoPad

--- app/picopad/build.sh
+++ app/picopad/build.sh
@@ -4,7 +4,9 @@ set -e
 
 ARG_DEVICE="picopad20"
 ARG_OUTDIR="bin"
-while getopts d:o: opt; do
+ARG_GRPDIR=""
+ARG_TARGET=""
+while getopts d:o:g:t: opt; do
   case ${opt} in
     d)
       ARG_DEVICE="${OPTARG}"
@@ -12,12 +14,28 @@ while getopts d:o: opt; do
     o)
       ARG_OUTDIR="${OPTARG}"
       ;;
+    g)
+      ARG_GRPDIR="${OPTARG}"
+      ;;
+    t)
+      ARG_TARGET="${OPTARG}"
+      ;;
     *)
       echo "Usage: $0 [-d device]"
       exit 1
       ;;
   esac
 done
+
+if [ -z "${ARG_GRPDIR}" ]; then
+  echo "*Error: -g GRPDIR is required."
+  exit 1
+fi
+
+if [ -z "${ARG_TARGET}" ]; then
+  echo "*Error: -t TARGET is required."
+  exit 1
+fi
 
 set -u
 
@@ -33,10 +51,8 @@ APP_DIR=$(pwd)
 REPO_DIR=$(cd ../.. && pwd)
 SUBMODULES_DIR="${REPO_DIR}/submodules"
 ADAFRUIT_GFX_DIR="${SUBMODULES_DIR}/Adafruit-GFX-Library"
-GRPDIR=PROG
-TARGET=FIXBROT
 
-SRC_TARGET_DIR="${APP_DIR}/${GRPDIR}/${TARGET}"
+SRC_TARGET_DIR="${APP_DIR}/${ARG_GRPDIR}/${ARG_TARGET}"
 LIB_DIR="${REPO_DIR}/lib"
 
 DEST_BIN_DIR="${APP_DIR}/bin/${ARG_DEVICE}"
@@ -51,8 +67,8 @@ pushdir "${PICOLIBSDK_PATH}/_tools"
 popdir
 
 pushdir "${PICOLIBSDK_PATH}/PicoPad"
-  mkdir -p "./${GRPDIR}/${TARGET}/src"
-  pushdir "./${GRPDIR}/${TARGET}"
+  mkdir -p "./${ARG_GRPDIR}/${ARG_TARGET}/src"
+  pushdir "./${ARG_GRPDIR}/${ARG_TARGET}"
     cp -rp ${LIB_DIR}/include/* "."
     cp -rp ${SRC_TARGET_DIR}/* "."
     cp -p "${ADAFRUIT_GFX_DIR}/gfxfont.h" "."
